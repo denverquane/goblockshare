9754ab3e58a66212c7c89339841bebf7f370eaba
denverquane <dquane@unm.edu>
2018-09-13 10:08:13 -0600

    Fixed out-of-range index when failing to load file for mock blockchain

--- blockchain/mockBlockchain.go
+++ blockchain/mockBlockchain.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (chain *BlockChain) AddMockTransactions() {
-	torr, _ := common.MakeTorrentFileFromFile(1000, "README.md", "readme")
+	torr, fileErr := common.MakeTorrentFileFromFile(1000, "READE.md", "readme")
 	for i := 0; i < 3; i++ {
 		address := common.GenerateNewPersonalAddress()
 		trans := common.PublishTorrentTrans{torr}
@@ -46,36 +46,36 @@ func (chain *BlockChain) AddMockTransactions() {
 			}
 		}
 
-
 		address3 := common.GenerateNewPersonalAddress()
 		address4 := common.GenerateNewPersonalAddress()
-		trans3 := common.SharedLayerTrans{torr.LayerHashKeys[0], address4.Address}
-		origin3 := address3.ConvertToOriginInfo()
-		btt3 := common.SignableTransaction{origin3, trans3, common.SHARED_LAYER, nil, nil, ""}
-		signed3 := btt3.SignAndSetTxID(&address3.PrivateKey)
-		log.Println("Gonna broadcast " + signed3.TxID + " to blockchains")
-		worked, err = chain.AddTransaction(signed3, "test addr", nil)
-		if !worked {
-			log.Println(err.Error())
-		}
+		if fileErr == nil {
+			trans3 := common.SharedLayerTrans{torr.LayerHashKeys[0], address4.Address}
+			origin3 := address3.ConvertToOriginInfo()
+			btt3 := common.SignableTransaction{origin3, trans3, common.SHARED_LAYER, nil, nil, ""}
+			signed3 := btt3.SignAndSetTxID(&address3.PrivateKey)
+			log.Println("Gonna broadcast " + signed3.TxID + " to blockchains")
+			worked, err = chain.AddTransaction(signed3, "test addr", nil)
+			if !worked {
+				log.Println(err.Error())
+			}
+			for chain.IsProcessing() {
+				time.Sleep(100)
+			}
 
-		for chain.IsProcessing() {
-			time.Sleep(100)
-		}
-
-		trans4 := common.LayerRepTrans{signed3.TxID, torr.LayerHashKeys[0],
-			true, true}
-		origin4 := address4.ConvertToOriginInfo()
-		btt4 := common.SignableTransaction{origin4, trans4, common.LAYER_REP, nil, nil, ""}
-		signed4 := btt4.SignAndSetTxID(&address4.PrivateKey)
-		log.Println("Gonna broadcast " + signed4.TxID + " to blockchains")
-		worked, err = chain.AddTransaction(signed4, "test addr", nil)
-		if !worked {
-			log.Println(err.Error())
+			trans4 := common.LayerRepTrans{signed3.TxID, torr.LayerHashKeys[0],
+				true, true}
+			origin4 := address4.ConvertToOriginInfo()
+			btt4 := common.SignableTransaction{origin4, trans4, common.LAYER_REP, nil, nil, ""}
+			signed4 := btt4.SignAndSetTxID(&address4.PrivateKey)
+			log.Println("Gonna broadcast " + signed4.TxID + " to blockchains")
+			worked, err = chain.AddTransaction(signed4, "test addr", nil)
+			if !worked {
+				log.Println(err.Error())
+			}
 		}
 
 		trans5 := common.SetAliasTrans{"Sample_Alias!"}
-		btt5 := common.SignableTransaction{origin4, trans5, common.SET_ALIAS, nil, nil, ""}
+		btt5 := common.SignableTransaction{address4.ConvertToOriginInfo(), trans5, common.SET_ALIAS, nil, nil, ""}
 		signed5 := btt5.SignAndSetTxID(&address4.PrivateKey)
 		log.Println("Gonna broadcast " + signed5.TxID + " to blockchains")
 		worked, err = chain.AddTransaction(signed5, "test addr", nil)
